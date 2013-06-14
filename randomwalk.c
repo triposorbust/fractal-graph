00d038261ed9dd4abe0fa1507913730b07fb3b5b
andy c <t2ahc@scientific-panda.local>
2013-06-14 14:43:05 -0500

    build randomwalk function

--- randomwalk.c
+++ randomwalk.c
@@ -4,7 +4,7 @@
 
 void random_walk(walk *walks, int end)
 {
-  int i,j,p;
+  int i,j,next_node;
   walk awalk;
   node anode;
 
@@ -13,13 +13,15 @@ void random_walk(walk *walks, int end)
     awalk = walks[i];
     anode = NODES[awalk.loc];
 
-    for (j=0; anode.edges[j] != -1; ++j) {
-      ; // Do the probability thing.
-    }
+    next_node = anode.edges[0];
+    for (j=0; anode.edges[j] != -1; ++j)
+      next_node = ((rand() % 100) / 100.0) <= (1.0 / (float)j) ? \
+        anode.edges[j] : next_node;
 
-    ; // Do some other stuff.
+    awalk.loc = next_node;
+    awalk.step += 1;
+    awalk.x = NODES[awalk.loc].x;
+    awalk.y = NODES[awalk.loc].y;
 
   }
-
-  return;
 }
